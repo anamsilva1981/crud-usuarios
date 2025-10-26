4395c5228ab16fc51493966618fb2b5973d36013
anamsilva1981 <ana.msilva@hotmail.com>
2025-10-26 19:14:04 -0300

    feat(interface): adiciona interface users

---
+++ src/app/interfaces/user.ts
@@ -0,0 +1,9 @@
+export interface User {
+    name: string;
+    email: string;
+    sector: string;
+    role: string;
+    fibrebaseId?: string;
+    healthPlan?: string;
+    dentalPlan?: string;
+   }
--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { Observable } from 'rxjs';
+import { User } from '../interfaces/user';
 
 @Injectable({
   providedIn: 'root'
@@ -15,11 +16,11 @@ export class UsersService {
     return this.dataBaseStore.collection('users', user => user.orderBy('name')).valueChanges({idField: 'firebaseId'}) as Observable<any[]>;
   }
 
-  addUser(user: any){
+  addUser(user: User){
     return this.dataBaseStore.collection('users').add(user);
   }
 
-  updateUser(userId: string, user: any){
+  updateUser(userId: string, user: User){
     return this.dataBaseStore.collection('users').doc(userId).update(user)
   }
 
